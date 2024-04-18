fb48dffa1acea8fc18b34cc261bbc077d165e48e
Zeyad Tarek <zeyad952004@gmail.com>
2024-04-18 22:29:18 +0200

    build: Start a Transactions page

---
+++ transactions.php
@@ -0,0 +1,19 @@
+<?php require_once("main-components/header.php")?>
+<?php require_once("main-components/side-navbar.php")?>
+<?php require_once("main-components/navbar.php")?>
+
+
+
+
+
+<!-- Blank Start -->
+<div class="container-fluid pt-4 px-4">
+    <div class="row vh-100 bg-secondary rounded align-items-center justify-content-center mx-0">
+        <div class="col-md-6 text-center">
+            <h3>This is Transactions page</h3>
+        </div>
+    </div>
+</div>
+<!-- Blank End -->
+
+<?php require_once("main-components/footer.php")?>
