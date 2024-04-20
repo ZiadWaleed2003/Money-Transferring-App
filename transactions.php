27e02f19b7389d5d65bef8d6784b8456139659f0
Zeyad Tarek <zeyad952004@gmail.com>
2024-04-20 02:17:43 +0200

    refactor: Improve HTML and CSS code

--- transactions.php
+++ transactions.php
@@ -6,10 +6,6 @@
 <!-- Transaction Start -->
 <div class="container-fluid pt-4 px-4">
     <div class="row bg-secondary rounded align-items-center justify-content-center mx-0 p-5">
-        
-        <div class="container back-to-previous">
-            <a href="" class="text-decoration-underline"><i class="bi bi-chevron-left me-1"></i>back</a>
-        </div>
 
         <div class="col-12 text-center header">
             <h3 class="display-3 p-5">Transactions</h3>
@@ -17,24 +13,16 @@
         
         <div class="col-12 text-center pb-5">
             <span id="links-tracking">
-                <a href="#" class="h5">test1</a>
-                &nbsp;
-                /
-                &nbsp;
-                <a href="#" class="h5">test2</a>
-                &nbsp;
-                /
-                &nbsp;
-                <a href="#" class="h5 text-primary">test3</a>
+                <a href="transactions.php" class="h5 active">Home Page</a>
 
             </span>
         </div>
 
         <div id="transaction-section-content" class="container-fluid pt-5">
-            <div>
+            <div class="home-page">
                 <div class="row g-4 mb-4">
                     <div class="col-sm-6 col-xl-3">
-                        <a href="#" class="options">
+                        <a id="check-balance" class="options" href="#">
                             <div class="bg-dark flex-column align-items-center justify-content-center p-4">
                                 <h3 class="m-0 text-center p-2">Check Balance</h3>
                             </div>
@@ -42,7 +30,7 @@
                     </div>
                     
                     <div class="col-sm-6 col-xl-3">
-                        <a href="#" class="options">
+                        <a id="send-to-card" class="options" href="#">
                             <div class="bg-dark flex-column align-items-center justify-content-center p-4">
                                 <h4 class="m-0 text-center p-2">Send Money to Card</h4>
                             </div>
@@ -50,15 +38,15 @@
                     </div>
                     
                     <div class="col-sm-6 col-xl-3">
-                        <a href="#" class="options">
+                        <a id="send-to-wallet" class="options" href="#">
                             <div class="bg-dark flex-column align-items-center justify-content-center p-4">
                                 <h4 class="m-0 text-center p-2">Send Money to Wallet</h4>
                             </div>
                         </a>
-                    </div>
+                    </div>  
                     
                     <div class="col-sm-6 col-xl-3">
-                        <a href="#" class="options">
+                        <a id="send-donation" class="options" href="#">
                             <div class="bg-dark flex-column align-items-center justify-content-center p-4">
                                 <h3 class="m-0 text-center p-2">Send Donation</h3>
                             </div>
@@ -69,23 +57,7 @@
                 <div class="row g-4 mb-4">
                     
                     <div class="col-sm-6 col-xl-3">
-                        <a href="#" class="options">
-                            <div class="bg-dark flex-column align-items-center justify-content-center p-4">
-                                <h3 class="m-0 text-center p-2">Request Money</h3>
-                            </div>
-                        </a>
-                    </div>
-    
-                    <div class="col-sm-6 col-xl-3">
-                        <a href="#" class="options">
-                            <div class="bg-dark flex-column align-items-center justify-content-center p-4">
-                                <h3 class="m-0 text-center p-2">Requests list</h3>
-                            </div>
-                        </a>
-                    </div>
-                    
-                    <div class="col-sm-6 col-xl-3">
-                        <a href="#" class="options">
+                        <a id="pay-payments" class="options" href="#">
                             <div class="bg-dark flex-column align-items-center justify-content-center p-4">
                                 <h3 class="m-0 text-center p-2">Pay Payments</h3>
                             </div>
@@ -93,9 +65,25 @@
                     </div>
                     
                     <div class="col-sm-6 col-xl-3">
-                        <a href="#" class="options">
+                        <a id="request-money" class="options" href="#">
                             <div class="bg-dark flex-column align-items-center justify-content-center p-4">
-                                <h3 class="m-0 text-center p-2">Transaction History</h3>
+                                <h3 class="m-0 text-center p-2">Request Money</h3>
+                            </div>
+                        </a>
+                    </div>
+    
+                    <div class="col-sm-6 col-xl-3">
+                        <a id="requests-list" class="options" href="#">
+                            <div class="bg-dark flex-column align-items-center justify-content-center p-4">
+                                <h3 class="m-0 text-center p-2">Requests list</h3>
+                            </div>
+                        </a>
+                    </div>
+                    
+                    <div class="col-sm-6 col-xl-3">
+                        <a id="transactions-history" class="options" href="#">
+                            <div class="bg-dark flex-column align-items-center justify-content-center p-4">
+                                <h3 class="m-0 text-center p-2">Transactions History</h3>
                             </div>
                         </a>
                     </div>
