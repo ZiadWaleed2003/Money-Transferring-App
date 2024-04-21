e08f4869308c837bf65424356745e15c6c895798
zyad atef <zyadatefali12@gmail.com>
2024-04-21 21:09:24 +0200

    Revert: commented a line in navbar

--- main-components/side-navbar.php
+++ main-components/side-navbar.php
@@ -15,7 +15,7 @@
                     </div>
                 </div>
                 <div class="navbar-nav w-100">
-                   <a id="admin" href="Admin.php" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>Blank</a>
+                   <!-- <a id="admin" href="Admin.php" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>admin</a> -->
                     <a id="card" href="card.php" class="nav-item nav-link"><i class="bi bi-arrow-down-up" style="transform: rotate(45deg);"></i> Cards</a>
                     <a id="transactions" href="transactions.php" class="nav-item nav-link"><i class="bi bi-arrow-down-up" style="transform: rotate(45deg);"></i> Transactions</a>
                     <a id="blank" href="blank.php" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>Blank</a>
