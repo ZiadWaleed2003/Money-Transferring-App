dc043b0edd89634fe57118c5769d7d341f0e71c5
Zeyad Tarek <zeyad952004@gmail.com>
2024-04-22 03:36:49 +0200

    Chore: Some Pages style changes at Transactions section

--- requistlist.php
+++ requistlist.php
@@ -1,6 +1,6 @@
-<?php require_once("main-components/header.php")?>
-<?php require_once("main-components/side-navbar.php")?>
-<?php require_once("main-components/navbar.php")?>
+<?php require_once("main-components/header.php") ?>
+<?php require_once("main-components/side-navbar.php") ?>
+<?php require_once("main-components/navbar.php") ?>
 
 
 
@@ -8,87 +8,92 @@
 
 <!-- Blank Start -->
 <div class="container-fluid pt-4 px-4">
-    <div class="row vh-100 bg-secondary rounded align-items-center justify-content-center mx-0">
-    <div class="">
-    <div class="bg-secondary rounded h-100 p-4">
-        <h6 class="mb-4">requist list</h6>
-        <table class="table table-dark">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col"> Name</th>
-                    <th scope="col">Amount</th>
-                    <th scope="col">description of requist</th>
-                    
-                </tr>
+    <div class="row bg-secondary rounded align-items-center justify-content-center mx-0 p-5">
+        <div class="container back-to-previous" onclick="window.history.back()">
+            <a class="text-decoration-underline"><i class="bi bi-chevron-left me-1"></i>back</a>
+        </div>
 
-                <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Check Balance</title>
-    
-</head>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>zeyad hussam </td>
-                    <td>300</td>
-                    <td>i want this money </td>
-                    <td><botton class="refuse">refuse</botton>
-                    </td>
-                    <td><botton class="accept">accept</botton>
-                    </td>
-                    <style>
-        /* CSS rules to change button colors */
-            .refuse{
-                border 1 px solid #700;
-                padding :10px;
-                color:fff;
-                background: #700;
-                border-radius: 2px;
-                transition: all 0.5;
-                font-size:10px;
-            }
-            .accept{
-                border 1 px solid #700;
-                padding :10px;
-                color:fff;
-                background: #008000;
-                border-radius: 2px;
-                transition: all 0.5;
-                font-size:10px;
-            }
+        <div class="col-12 text-center header">
+            <h3 class="display-3 p-5">Transactions</h3>
+        </div>
 
-    </style></td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>mohamed</td>
-                    <td>30000</td>
-                    <td>xxxxxxxxxxxxxxxxxxx</td>
-                    <td><botton class="refuse">refuse</botton>
-                    </td>
-                    <td><botton class="accept">accept</botton>
-                    </td>
-                    
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>adel</td>
-                    <td>6000</td>
-                    <td>zzzzzzzzzzzzzzzzz</td>
-                    <td><botton class="refuse">refuse</botton>
-                    </td>
-                    <td><botton class="accept">accept</botton>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-</div>
+        <div class="col-12 text-center pb-5">
+            <span id="links-tracking">
+                <a href="transactions.php" class="h5">Home Page</a>
+                &nbsp;
+                /
+                &nbsp;
+                <a href="requistlist.php" class="h5 text-primary">Requests List</a>
+
+            </span>
+        </div>
+
+        <div class="container-fluid pt-5">
+            <div class="row justify-content-center h-100 p-5">
+                <div class="">
+                    <div class="bg-secondary rounded h-100 p-4">
+                        
+                        <div class="table-responsive bg-dark p-5">
+
+                            <table class="table table-dark table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col"> Name</th>
+                                        <th scope="col">Amount</th>
+                                        <th scope="col">description of requist</th>
+                                        <th scope="col"></th>
+                                        <th scope="col"></th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>zeyad hussam </td>
+                                        <td>300</td>
+                                        <td>i want this money </td>
+                                        <td>
+                                            <button class="btn btn-success m2">accept</button>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-danger m2">refuse</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>mohamed</td>
+                                        <td>30000</td>
+                                        <td>xxxxxxxxxxxxxxxxxxx</td>
+                                        <td>
+                                            <button class="btn btn-success m2">accept</button>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-danger m2">refuse</button>
+                                        </td>
+
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>adel</td>
+                                        <td>6000</td>
+                                        <td>zzzzzzzzzzzzzzzzz</td>
+                                        <td>
+                                            <button class="btn btn-success m2">accept</button>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-danger m2">refuse</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <!-- Blank End -->
 
-<?php require_once("main-components/footer.php")?>
+<?php require_once("main-components/footer.php") ?>
