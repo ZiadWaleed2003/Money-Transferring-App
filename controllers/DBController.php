0b7a8a8b8a09c129cbb0b2f961500cc462f84556
ZIad Adel <120601753+AbuElzozz@users.noreply.github.com>
2024-04-25 22:42:30 +0200

    add model card and product controller

--- controllers/DBController.php
+++ controllers/DBController.php
@@ -22,7 +22,7 @@ class DBController
 
     public function closeconnection(){
         if($this->connection){
-            $this->connection.close();
+            $this->connection->close();
         }
         else{
             echo "Connection not open";
@@ -40,5 +40,35 @@ class DBController
             return $result->fetch_all(MYSQLI_ASSOC);
         }
     }
+    public function insert($qry)
+    {
+        $result=$this->connection->query($qry);
+        if(!$result)
+        {
+            echo "Error : ".mysqli_error($this->connection);
+            return false;
+        }
+        else
+        {
+             return $this->connection->insert_id;
+        }
 
+    }
+    public function delete($qry)
+    {
+        $result=$this->connection->query($qry);
+        if(!$result)
+        {
+            echo "Error : ".mysqli_error($this->connection);
+            return false;
+        }
+        else
+        {
+             return $result;
+        }
+
+ 
+
+    }
 }
+?>
