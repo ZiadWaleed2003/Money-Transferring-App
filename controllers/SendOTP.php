7fc42248c28fd58e10297baffb89f1037825467d
Ziad Waleed <ziadwaleedmohamed2003@gmail.com>
2024-04-27 02:04:31 +0300

    Feat (OTP) : Working finally but in temp state i'll modify it later

--- controllers/SendOTP.php
+++ controllers/SendOTP.php
@@ -3,38 +3,41 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-// require 'path/to/PHPMailer/src/Exception.php';
-// require 'path/to/PHPMailer/src/PHPMailer.php';
-// require 'path/to/PHPMailer/src/SMTP.php';
+
 
 require '../mailer/autoload.php';
 
-$mail = new PHPMailer();
 
-    $mail->SMTPDebug = 3;
+
+$mail = new PHPMailer(true); //defaults to using php "mail()"; the true param means it will throw exceptions on errors, which we need to catch
+
+try {
+
+    $mail->SMTPDebug = 0;
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'elzozatandbasselco@gmail.com';
-    $mail->Password = '123456789elzozatandbassel';
+    $mail->Password = 'zwdajtpmlvvgvmij';
     $mail->SMTPSecure = 'tls';
-    $mail->Port = 465;
+    $mail->Port = 587;
 
-    $mail->setFrom('elzozatandbasselco@gmail.com' , 'ElZozat and Bassel CO.');
-    $mail->addAddress("ziadwaleedmohamed2003@gmail.com");
-
-
-    $mail->isHTML(true);
-    $mail->Subject = "Your Verification code";
-
-    $mail->Body = "<p>Your verification code is 1234</p>";
+        $mail->setFrom('elzozatandbasselco@gmail.com' , 'ElZozat and Bassel CO.');
+        $mail->addAddress("shahersa3ed1@gmail.com");
 
 
-    if($mail->send()){
+        $mail->isHTML(true);
+        $mail->Subject = "Your Verification code";
 
-        echo"Email sent";
-    }else{
-        echo "Email failed to sent";
+        $mail->Body = "<p>ana 3ayez bosa ya baby</p>";
+
+ 
+        $mail->Send();
+        echo "Message Sent OK\n";
+    } catch (Exception $e) {
+    echo $e->errorMessage(); //Pretty error messages from PHPMailer
+    } catch (Exception $e) {
+    echo $e->getMessage(); //Boring error messages from anything else!
     }
 
 
--- controllers/testOTP.php
+++ controllers/testOTP.php
@@ -1,6 +1,6 @@
 <?php
 
-header("location : SendOTP.php");
+header("location:SendOTP.php");
 
 
 ?>
