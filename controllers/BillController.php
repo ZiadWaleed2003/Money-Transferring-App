46aa3b38637e3e7f098fb48bed006d9e75a69217
zyad atef <zyadatefali12@gmail.com>
2024-04-26 22:02:13 +0300

    style

--- controllers/BillController.php
+++ controllers/BillController.php
@@ -13,28 +13,22 @@ class BillController{
             $billname = $bill->getBillName();
             $acct_no = $bill->getAcct_No();
             
-            // Query to check if the bank with the given name and ID already exists
             $check ="SELECT * FROM bills WHERE name = '$billname' AND account_number ='$acct_no'";
             
-            // Performing the query to check for existing banks
             $existingBills = CRUD::Select($check);
             
-            // Checking if there are no existing banks with the same name and ID
             if(empty($existingBills)){
-                // Query to insert the new bank into the database
                 $query = "INSERT INTO bills (name, account_number,balance) VALUES ('$billname', '$acct_no', '0.0')";
                 
-                // Performing the query to insert the new bank
                 $result = CRUD::Insert($query);
                 
-                // Checking if the insertion was successful
                 if($result !== false){
-                    return true; // Bank successfully added
+                    return true; 
                 } else {
-                    return false; // Bank not added due to insertion failure
+                    return false;
                 }
             } else {
-                return false; // Bank not added because a bank with the same name and ID already exists
+                return false; 
             }
         }
     }
