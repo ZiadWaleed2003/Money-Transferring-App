6989e59de4cde9c38c3f2c1f2af99bdde4c8d408
zyad atef <zyadatefali12@gmail.com>
2024-04-26 03:23:24 +0300

    feat : added bank class and BankController + Admin index 34an adel <3

---
+++ Models/bank.php
@@ -0,0 +1,22 @@
+<?php
+
+class bank{
+    private $name;
+    private $id;
+    protected $db;
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($name){
+        $this->name = $name;
+    }
+    public function getId(){
+        return $this->id;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+    
+}
+
+?>
