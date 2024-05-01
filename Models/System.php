31ec35e1badc2db0debce2ea9cd9bee10715e23c
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2024-05-01 13:45:16 +0300

    chore: db-data ignore

--- Models/System.php
+++ Models/System.php
@@ -9,15 +9,17 @@ use PHPMailer\PHPMailer\Exception;
 require '../mailer/autoload.php';
 
 
-class System{
+class System
+{
 
 
-    public static function SendOTP($user_email){
+    public static function SendOTP($user_email)
+    {
 
         $mail = new PHPMailer(true); //defaults to using php "mail()"; the true param means it will throw exceptions on errors, which we need to catch
 
         try {
-        
+
             $mail->SMTPDebug = 0;
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
@@ -26,15 +28,15 @@ class System{
             $mail->Password = 'zwdajtpmlvvgvmij';
             $mail->SMTPSecure = 'tls';
             $mail->Port = 587;
-        
-            $mail->setFrom('elzozatandbasselco@gmail.com' , 'ElZozat and Bassel CO.');
+
+            $mail->setFrom('elzozatandbasselco@gmail.com', 'ElZozat and Bassel CO.');
             $mail->addAddress($user_email);
             $mail->isHTML(true);
             $mail->Subject = "Your Verification code";
 
             $otp = System::generateOTP(6);
-        
-                $mail->Body = "
+
+            $mail->Body = "
                         <html>
                         <head>
                         <title>Your One-Time Password (OTP)</title>
@@ -45,47 +47,49 @@ class System{
                         <h1 style='font-size: 24px; font-weight: bold; text-align: center;'>[$otp]</h1>
                         </body>
                         </html>";
-        
-         
-                $mail->Send();
 
-                $_SESSION['otp'] = $otp;
 
-                echo "Message Sent OK\n";
-        
-            } catch ( Exception $e) {
-        
-                echo $e->errorMessage(); //Pretty error messages from PHPMailer
-            
-            }
+            $mail->Send();
+
+            $_SESSION['otp'] = $otp;
+
+            echo "Message Sent OK\n";
+
+        } catch (Exception $e) {
+
+            echo $e->errorMessage(); //Pretty error messages from PHPMailer
+
+        }
 
     }
 
 
 
-    private static function  generateOTP($n) { 
-      
+    private static function generateOTP($n)
+    {
+
         // Take a generator string which consist of 
         // all numeric digits 
-        $generator = "1357902468"; 
-      
+        $generator = "1357902468";
+
         // Iterate for n-times and pick a single character 
         // from generator and append it to $result 
-          
+
         // Login for generating a random character from generator 
         //     ---generate a random number 
         //     ---take modulus of same with length of generator (say i) 
         //     ---append the character at place (i) from generator to result 
-      
-        $result = ""; 
-      
-        for ($i = 1; $i <= $n; $i++) { 
-            $result .= substr($generator, (rand()%(strlen($generator))), 1); 
-        } 
-      
+
+        $result = "";
+
+        for ($i = 1; $i <= $n; $i++) {
+            $result .= substr($generator, (rand() % (strlen($generator))), 1);
+        }
+
         // Return result 
-        return $result; 
-    } 
+        return $result;
+    }
 }
 
 ?>
+
