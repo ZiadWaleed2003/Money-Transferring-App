88d50a3990eab0d34e0a2235011c5aa9e94e1643
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2024-05-01 16:29:28 +0300

    chore: refactor upload pic into a func in user to allow register to upload pic

--- Models/User.php
+++ Models/User.php
@@ -77,6 +77,38 @@ class User extends PERSON
         CRUD::Delete($query);
     }
 
+    public function uploadPicture($file)
+    {
+        $upl_dir = $_SERVER['DOCUMENT_ROOT'] . "/views/assets/img/";
+        $file_path = $upl_dir . basename($file["name"]);
+        $file_type = strtolower(pathinfo($file_path, PATHINFO_EXTENSION));
+        $trgt = $this->id . "-img.";
+        $final = $upl_dir . $trgt . $file_type;
+        $uploadOk = 1;
+        $check = getimagesize($_FILES["profile_img"]["tmp_name"]);
+        if ($check !== false) {
+            $uploadOk = 1;
+        } else {
+            $uploadOk = 0;
+        }
+        if (
+            $file_type != "jpg" && $file_type != "png" && $file_type != "jpeg"
+            && $file_type != "gif"
+        ) {
+            $uploadOk = 0;
+        }
+        if ($uploadOk == 0) {
+        } else {
+            $file_pattern = $upl_dir . $trgt . "*";
+            array_map("unlink", glob($file_pattern));
+            if (move_uploaded_file($_FILES["profile_img"]["tmp_name"], $final)) {
+                return "/views/assets/img/" . $trgt . $file_type;
+            } else {
+                echo "Upload Error Occured";
+            }
+        }
+    }
+
     public function setImagePath($image_path)
     {
 
