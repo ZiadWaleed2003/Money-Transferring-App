231fe8ca0f4c32d645670e58bb3743ff462a8c25
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2024-05-08 22:54:06 +0300

    feat: add signup clarification

--- Views/auth/signup.php
+++ Views/auth/signup.php
@@ -24,7 +24,8 @@ if (isset($_SESSION['admin'])) {
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap"
+        rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
@@ -44,7 +45,8 @@ if (isset($_SESSION['admin'])) {
 <body>
     <div class="container-fluid position-relative d-flex p-0">
         <!-- Spinner Start -->
-        <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div id="spinner"
+            class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
                 <span class="sr-only">Loading...</span>
             </div>
@@ -63,34 +65,43 @@ if (isset($_SESSION['admin'])) {
                             </a>
                             <h3>Sign Up</h3>
                         </div>
-
-                        <form action="../../controllers/SignUpController.php" method="post" enctype="multipart/form-data" id="SignUpForm">
+                        <form action="../../controllers/SignUpController.php" method="post"
+                            enctype="multipart/form-data" id="SignUpForm">
                             <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="floatingText" placeholder="jhondoe" name="username" required>
+                                <input type="text" class="form-control" id="floatingText" placeholder="jhondoe"
+                                    name="username" required>
                                 <label for="floatingText">Username</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email" required>
+                                <input type="email" class="form-control" id="floatingInput"
+                                    placeholder="name@example.com" name="email" required>
                                 <label for="floatingInput">Email address</label>
                             </div>
                             <div class="form-floating mb-4">
-                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password" required>
+                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                                    name="password" required>
                                 <label for="floatingPassword">Password</label>
                             </div>
                             <div class="form-floating mb-4">
-                                <input type="password" class="form-control" id="floatingPassword" placeholder="Confirm Password" name="ConfirmPassword" required>
+                                <input type="password" class="form-control" id="floatingPassword"
+                                    placeholder="Confirm Password" name="ConfirmPassword" required>
                                 <label for="floatingPassword">Confirm Password</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="floatingText" placeholder="1234567890" name="phone" required>
+                                <input type="text" class="form-control" id="floatingText" placeholder="1234567890"
+                                    name="phone" required>
                                 <label for="floatingText">Phone Number</label>
                             </div>
                             <!-- File input for image -->
                             <div class="mb-3">
-                                <label for="image" class="form-label">Profile Image</label>
-                                <input class="form-control" type="file" id="image" name="image">
-                                <br>
-                                <button type="submit" name="submit" class="btn btn-primary py-3 w-100 mb-4">Sign Up</button>
+                                <label for="image" class="form-label">FaceID Image*</label>
+                                <input class="form-control mb-2" type="file" id="image" name="image">
+                                <small class=" text-muted">*: This image will only be used for login purposes. For a
+                                    profile
+                                    image
+                                    upload after signup</small>
+                            </div>
+                            <button type="submit" name="submit" class="btn btn-primary py-3 w-100 mb-4">Sign Up</button>
                         </form>
                         <p class="text-center mb-0">Already have an Account? <a href="signin.php">Sign In</a></p>
                     </div>
