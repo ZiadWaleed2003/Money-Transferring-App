4d5355a7673e5c97e9ab37e5035c35ad11e43611
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2024-05-08 22:13:47 +0300

    chore: format

--- controllers/CardController.php
+++ controllers/CardController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once "CRUD.php";
 require_once "DBConnection.php";
@@ -9,138 +9,130 @@ class CardController
 
     public function getbanks()
     {
-         
-            $query="SELECT * FROM bank ORDER BY id ASC ";
-            $result=CRUD::Select($query);
-            if($result){
+
+        $query = "SELECT * FROM bank ORDER BY id ASC ";
+        $result = CRUD::Select($query);
+        if ($result) {
             return $result;
-            }
-            else{
-                echo"error";
-            }
-         }
+        } else {
+            echo "error";
+        }
+    }
 
-         public function addCard(Card $card)
-             {
-            $bank_id=$card->getBankId();
-            $number=$card->getNumber();
-            $cvv=$card->getCvv();
-            $check = "SELECT * From bankcards where bankcards.number ='$number' and bankcards.bank_id='$bank_id' and bankcards.cvv='$cvv' and bankcards.user_mobile_number='".$_SESSION['user']['phone_number']."' ";
-            $variable=CRUD::Select($check);  
-            if ($variable !=false){ 
-                $query2="SELECT * From usercards where usercards.number ='$number' and usercards.user_id=".$_SESSION["user"]["id"]." and usercards.bank_id='$bank_id' ";
-                $result2=CRUD::Select($query2);
-                if(!count($result2)){
-                $cardname=$card->getName();
-                $cardnumber=$card->getNumber();
-                $cardcvv=$card->getCvv();
-                $cardipn=$card->getIpnCode();
-                $cardbalance=$variable[0]['balance'];          
-                $query="insert into usercards(user_id,bank_id,name,number,cvv,ipn_code,balance,favourite) values ('".$_SESSION["user"]["id"]."','$bank_id','$cardname','$cardnumber','$cardcvv','$cardipn','$cardbalance','0')";
-                $result=CRUD::Insert($query);
-                $_SESSION['success_message']="Card Added Successfully";
-                 return true;
-                }
-                else
-                {
-                    $_SESSION['error_message']="Card Added Before";
-                    return false;
-
-                }
-            }
-            else{
-                $_SESSION['error_message']="Card Not Found";
+    public function addCard(Card $card)
+    {
+        $bank_id = $card->getBankId();
+        $number = $card->getNumber();
+        $cvv = $card->getCvv();
+        $check = "SELECT * From bankcards where bankcards.number ='$number' and bankcards.bank_id='$bank_id' and bankcards.cvv='$cvv' and bankcards.user_mobile_number='" . $_SESSION['user']['phone_number'] . "' ";
+        $variable = CRUD::Select($check);
+        if ($variable != false) {
+            $query2 = "SELECT * From usercards where usercards.number ='$number' and usercards.user_id=" . $_SESSION["user"]["id"] . " and usercards.bank_id='$bank_id' ";
+            $result2 = CRUD::Select($query2);
+            if (!count($result2)) {
+                $cardname = $card->getName();
+                $cardnumber = $card->getNumber();
+                $cardcvv = $card->getCvv();
+                $cardipn = $card->getIpnCode();
+                $cardbalance = $variable[0]['balance'];
+                $query = "insert into usercards(user_id,bank_id,name,number,cvv,ipn_code,balance,favourite) values ('" . $_SESSION["user"]["id"] . "','$bank_id','$cardname','$cardnumber','$cardcvv','$cardipn','$cardbalance','0')";
+                $result = CRUD::Insert($query);
+                $_SESSION['success_message'] = "Card Added Successfully";
+                return true;
+            } else {
+                $_SESSION['error_message'] = "Card Added Before";
                 return false;
+
             }
-            
+        } else {
+            $_SESSION['error_message'] = "Card Not Found";
+            return false;
+        }
+
 
 
     }
-    public function getAllCards(){
-        $query="select usercards.id,bank.name as 'bank',usercards.name,usercards.number,favourite from usercards,bank where usercards.bank_id=bank.id AND usercards.user_id=".$_SESSION["user"]["id"]."";
-            $result=CRUD::Select($query);
-            if($result){
+    public function getAllCards()
+    {
+        $query = "select usercards.id,bank.name as 'bank',usercards.name,usercards.number,favourite from usercards,bank where usercards.bank_id=bank.id AND usercards.user_id=" . $_SESSION["user"]["id"] . "";
+        $result = CRUD::Select($query);
+        if ($result) {
             return $result;
-            }
-            else{
-                return false;
-            }
+        } else {
+            return false;
+        }
     }
 
 
     public function deleteCard($id)
     {
 
-       $query="delete from usercards where id=$id";
-       $result=CRUD::Delete($query);
-       if($result !=false){
-        return true;
-       }
-       else{
-        return false;
-       }
-        
-   }
+        $query = "delete from usercards where id=$id";
+        $result = CRUD::Delete($query);
+        if ($result != false) {
+            return true;
+        } else {
+            return false;
+        }
 
-   public function updateCard($name,$id)
+    }
+
+    public function updateCard($name, $id)
     {
-       $query="update usercards SET name='$name' where usercards.id=$id";
-       $result=CRUD::Update($query);
-       if($result !=false){
-        return true;
-       }
-       else{
-        return false;
-       }
-        
-   }
+        $query = "update usercards SET name='$name' where usercards.id=$id";
+        $result = CRUD::Update($query);
+        if ($result != false) {
+            return true;
+        } else {
+            return false;
+        }
 
-   public function favCard($id)
+    }
+
+    public function favCard($id)
     {
-       $query="update usercards SET usercards.favourite=1 where usercards.id=$id";
-       $result=CRUD::Update($query);
-       if($result !=false){
-        return true;
-       }
-       else{
-        return false;
-       }
-        
-   }
+        $query = "update usercards SET usercards.favourite=1 where usercards.id=$id";
+        $result = CRUD::Update($query);
+        if ($result != false) {
+            return true;
+        } else {
+            return false;
+        }
 
-   public function UndofavCard($id)
+    }
+
+    public function UndofavCard($id)
     {
-       $query="update usercards SET usercards.favourite=0 where usercards.id=$id";
-       $result=CRUD::Update($query);
-       if($result !=false){
-        return true;
-       }
-       else{
-        return false;
-       }
-        
-   }
+        $query = "update usercards SET usercards.favourite=0 where usercards.id=$id";
+        $result = CRUD::Update($query);
+        if ($result != false) {
+            return true;
+        } else {
+            return false;
+        }
 
-   public function getAllfavCards(){
-    $query="SELECT usercards.id, bank.name AS 'bank', usercards.name, usercards.number
+    }
+
+    public function getAllfavCards()
+    {
+        $query = "SELECT usercards.id, bank.name AS 'bank', usercards.name, usercards.number
     FROM usercards
     INNER JOIN bank ON usercards.bank_id = bank.id
     WHERE usercards.favourite = 1
-    AND usercards.user_id =".$_SESSION["user"]["id"]."
+    AND usercards.user_id =" . $_SESSION["user"]["id"] . "
      ";
-        $result=CRUD::Select($query);
-        if($result){
-        return $result;
-        }
-        else{
+        $result = CRUD::Select($query);
+        if ($result) {
+            return $result;
+        } else {
 
             return false;
         }
-}
+    }
 
 
 }
 
-    
 
-    
+
+
